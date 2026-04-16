a21189b8ac6d6c8ecfff0da607672fd895c4787b
Omar113650 <omarelhelaly520@gmail.com>
2026-04-17 00:04:48 +0200

    add auth & event module

---
+++ Module/auth/Contracts(DTOs)/RegisterRequest.cs
@@ -0,0 +1,9 @@
+namespace Eventix_Project.DTOs.Auth;
+
+public class RegisterRequest
+{
+    public string FullName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string ConfirmPassword { get; set; }
+}
